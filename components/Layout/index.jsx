e5454427a9d8fb121abce04f4d0ad52606046592
MyoMyintAung <myomyintaung064@gmail.com>
2022-11-02 13:56:53 +0630

    created layout components

---
+++ components/Layout/index.jsx
@@ -0,0 +1,38 @@
+import Head from "next/head";
+import Link from "next/link";
+import React from "react";
+
+const Layout = ({ children, title }) => {
+  return (
+    <>
+      <Head>
+        <title>{title ? title : "Let's Shop"}</title>
+        <meta
+          name="description"
+          content="World's best e-commerce platform where you can get everything"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="flex flex-col min-h-screen justify-between">
+        <header>
+          <nav className="h-12 shadow flex justify-between items-center px-4">
+            <Link href="/" className="text-lg font-bold">
+              Let`s Shop
+            </Link>
+            <div className="space-x-4">
+              <Link href="/cart">Cart</Link>
+              <Link href="/login">Login</Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="h-10 flex justify-center items-center text-neutral-500">
+          Copyright 2022 Let`s Shop
+        </footer>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
+import Head from "next/head";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold">Hello</h1>;
+  return (
+    <>
+      <Head>
+        <title>Home Page</title>
+      </Head>
+      <h1 className="text-3xl font-bold">Home Page</h1>
+    </>
+  );
 }
