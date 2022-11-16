c00b46ba04e886dc5aca7dbdc1c504e78260f3a1
MyoMyintAung <myomyintaung064@gmail.com>
2022-11-16 14:46:12 +0630

    fixed order cart table

--- components/LV2/Order/OrderInfo.jsx
+++ components/LV2/Order/OrderInfo.jsx
@@ -1,7 +1,7 @@
+import Image from "next/image";
 import Link from "next/link";
 import React, { useContext } from "react";
 import { Context } from "../../../Store/context";
-import CartTable from "../Cart/CartTable";
 
 const convertToStringAddress = (address) => {
   const stringAddress = `${address?.fullname}, ${address?.postal}, ${address?.address}, ${address?.city} ${address?.country}`;
@@ -36,7 +36,47 @@ const OrderInfo = () => {
       </div>
       <div className="card h-fit p-4 ">
         <h4 className="text-lg font-semibold">Order Items</h4>
-        <CartTable items={cartCtx.cart.cartItems} />
+        <table className="table-auto w-full mt-5">
+          <thead>
+            <tr>
+              <th className="text-left">Item</th>
+              <th>Quantity</th>
+              <th>Price</th>
+              <th>SubTotal</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cartCtx.cart.cartItems.map((item) => (
+              <tr key={item.slug}>
+                <td className="p-3 flex items-center ">
+                  <div className="w-20">
+                    <Image
+                      src={item.image}
+                      width={50}
+                      height={50}
+                      alt={item.name}
+                    ></Image>
+                  </div>
+
+                  <h3>{item.name}</h3>
+                </td>
+                <td className="p-3 text-center ">
+                  <p>{item.quantity}</p>
+                </td>
+                <td className="p-3 text-center">${item.price}</td>
+                <td className="p-3 text-center">
+                  <p>{item.quantity * item.price}</p>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <Link
+          href="/cart"
+          className="text-blue-500 cursor-pointer hover:text-blue-600 active:text-blue-800 mt-3"
+        >
+          Edit
+        </Link>
       </div>
     </div>
   );
